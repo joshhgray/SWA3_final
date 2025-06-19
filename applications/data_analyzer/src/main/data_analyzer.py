bcbca7aad7770e55179fd6c6ccf6ee9cbb35d927
Joshua Gray <jgray2997@gmail.com>
2025-06-18 22:10:08 -0400

    added more analysis endpoints

--- applications/data_analyzer/src/main/data_analyzer.py
+++ applications/data_analyzer/src/main/data_analyzer.py
@@ -1,5 +1,6 @@
 from applications.data_collector.src.main.data_collector import db, create_app, AdverseEvent, Drug, Reaction
 from flask import Flask, request, jsonify
+from sqlalchemy import case, func, desc
 import os
 
 app = create_app()
@@ -91,6 +92,82 @@ def most_common_reaction():
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
 
+"""
+Show drug usage across different age groups
+"""
+@app.route("/drug-by-age-groups/<drug_name>")
+def drug_by_age_groups(drug_name):
+    try:
+        # split into age groups - based on MeSH - https://pmc.ncbi.nlm.nih.gov/articles/PMC1794003/
+        age_groups = case([
+            (AdverseEvent.patient_age <= 2, "0-2"),
+            (AdverseEvent.patient_age <= 5, "3-5"),
+            (AdverseEvent.patient_age <= 12, "6-12"),
+            (AdverseEvent.patient_age <= 18, "13-18"),
+            (AdverseEvent.patient_age <= 44, "19-44"),
+            (AdverseEvent.patient_age <= 64, "45-64"),
+            (AdverseEvent.patient_age <= 79, "65-79"),
+            (AdverseEvent.patient_age >=80, "80+"),
+        ], else_="unknown").label("age_group")
+
+        results = (
+            db.session.query(age_groups, func.count().label("count"))
+            .select_from(AdverseEvent)
+            .join(Drug, Drug.event_id == AdverseEvent.safety_report_id)
+            .filter(Drug.drug_name.ilkike(f"%{drug_name}%"))
+            .group_by(age_groups)
+            .order_by(age_groups)
+            .all()
+        )
+
+        return jsonify({
+            "drug": drug_name,
+            "age_distribution": [{"age_group": r[0], "count": r[1]} for r in results]
+        })
+    
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+    
+"""
+Show top reactions per age group for a given drug
+"""
+@app.route("/top-reactions-by-age/<drug_name>")
+def drug_by_age_groups(drug_name):
+    try:
+        # split into age groups - based on MeSH - https://pmc.ncbi.nlm.nih.gov/articles/PMC1794003/
+        age_groups = case([
+            (AdverseEvent.patient_age <= 2, "0-2"),
+            (AdverseEvent.patient_age <= 5, "3-5"),
+            (AdverseEvent.patient_age <= 12, "6-12"),
+            (AdverseEvent.patient_age <= 18, "13-18"),
+            (AdverseEvent.patient_age <= 44, "19-44"),
+            (AdverseEvent.patient_age <= 64, "45-64"),
+            (AdverseEvent.patient_age <= 79, "65-79"),
+            (AdverseEvent.patient_age >=80, "80+"),
+        ], else_="unknown").label("age_group")
+
+        results = (
+            db.session.query(age_groups, Reaction.reaction_medical_term, func.count().label("count"))
+            .join(Drug, Drug.event_id == AdverseEvent.safety_report_id)
+            .filter(Drug.drug_name.ilkike(f"%{drug_name}%"))
+            .group_by(age_groups, Reaction.reaction_medical_term)
+            .order_by(age_groups, desc("count"))
+            .all()
+        )
+
+        group_dict = {}
+        for age_group, reaction, count in results:
+            if age_group not in group_dict:
+                group_dict[age_group] = {"reaction": reaction, "count": count}
+
+        return jsonify({
+            "drug": drug_name,
+            "top_reaction_by_age_group": group_dict
+        })
+    
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
