c6e1e115fe06b86939b4b346c2d12d5053e16a37
Joshua Gray <jgray2997@gmail.com>
2025-06-20 18:46:05 -0400

    add env vars and fix port binding for heroku

--- applications/frontend/src/main/app.py
+++ applications/frontend/src/main/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import dash
 from dash import dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
-import requests
+import dash
+import os
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
 
@@ -50,6 +50,7 @@ def update_output(n_clicks, drug_name):
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
 
